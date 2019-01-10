5a5d9fcbbbf9affd665c5baa29d0c86b027aa903
moskowitza <moskowitzaaron@mac.com>
2019-01-10 07:40:29 -0500

    ok

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Me</h1>
+      <p>Aaron is learning how to use Gatsby.</p>
+    </div>
+  </Layout>
+)
+export default About
