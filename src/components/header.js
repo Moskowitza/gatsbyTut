9562bbc2e67885bdee7ece83bcbe3f95380c91ea
moskowitza <moskowitzaaron@mac.com>
2019-01-11 10:46:23 -0500

    introduced styled components

--- src/components/header.js
+++ src/components/header.js
@@ -2,21 +2,24 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 import gatsbyLogo from '../images/gatsby-icon.png'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.div`
+  background: #bada55;
+  margin-bottom: 1.45rem;
+  img {
+    margin-bottom: 0;
+  }
+`
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1rem;
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <HeaderWrapper>
+    <HeaderContainer>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -34,8 +37,8 @@ const Header = ({ siteTitle }) => (
           />
         </Link>
       </h1>
-    </div>
-  </div>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 Header.propTypes = {
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,14 @@ import { StaticQuery, graphql } from 'gatsby'
 import Archive from './archive'
 import Header from './header'
 import './layout.css'
+import styled from 'styled-components'
 
+const MainLayout = styled.main`
+  max-width: 90%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+`
 const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
@@ -19,22 +26,15 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
+        <MainLayout>
+          <div>{children}</div>
           <Archive />
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <a href="https://www.five12c.com">Gatsby</a>
           </footer>
-        </div>
+        </MainLayout>
       </>
     )}
   />
